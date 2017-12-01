50dad9b811260168fd5fe525db2c55542a0fa598
4512125 <4512125@naver.com>
2017-12-01 23:34:32 +0900

    ads

--- map.js
+++ map.js
@@ -1,28 +1,204 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 10 // 지도의 확대 레벨
+        level: 3 // 지도의 확대 레벨
     };
 
 var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 var positions = [
     {
-        title: '카카오',
-        latlng: new daum.maps.LatLng(33.450705, 126.570677)
+        title: '시민화장실',
+        latlng: new daum.maps.LatLng(33.500042, 126.531944)
     },
     {
-        title: '생태연못',
-        latlng: new daum.maps.LatLng(33.450936, 126.569477)
+        title: '공항입구주차장',
+        latlng: new daum.maps.LatLng(33.503306, 126.495685)
     },
     {
-        title: '텃밭',
-        latlng: new daum.maps.LatLng(33.450879, 126.569940)
-    },
-    {
-        title: '근린공원',
+        title: '제일주차빌딩',
         latlng: new daum.maps.LatLng(33.451393, 126.570738)
-    }
+    },
+    {
+        title: '인제공영주차장',
+        latlng: new daum.maps.LatLng(33.504827, 126.541996)
+    },
+    {
+        title: '법원북측 공영주차장',
+        latlng: new daum.maps.LatLng(33.494713, 126.535350)
+    },
+    {
+        title: '모충사',
+        latlng: new daum.maps.LatLng(33.516430, 126.547003)
+    },
+    {
+        title: '충혼묘지 입구 (천왕사입구)',
+        latlng: new daum.maps.LatLng(33.516430, 126.547003)
+    },
+    {
+        title: '충혼묘지',
+        latlng: new daum.maps.LatLng(33.412640, 126.547003)
+    },
+    {
+        title: '묘지관리사무소 (충혼각 절)',
+        latlng: new daum.maps.LatLng(33.418898, 126.493132)
+    },
+    {
+        title: '노형신비의도로',
+        latlng: new daum.maps.LatLng(33.449879, 126.487336)
+    },
+    {
+        title: '관음사신비의도로',
+        latlng: new daum.maps.LatLng(33.435870, 126.554983)
+    },
+    {
+        title: '새별오름',
+        latlng: new daum.maps.LatLng(33.365380, 126.361954)
+    },
+    {
+        title: '새별오름',
+        latlng: new daum.maps.LatLng(33.365380, 126.361954)
+    },
+    {
+        title: '새별오름',
+        latlng: new daum.maps.LatLng(33.365380, 126.361954)
+    },
+    {
+        title: '실내수영장',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '실내체육관',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '애향운동장',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '야구장',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '연정정구장',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '주경기장',
+        latlng: new daum.maps.LatLng(33.497505, 126.515547)
+    },
+    {
+        title: '회천 파크골프장',
+        latlng: new daum.maps.LatLng(33.471863, 126.636599)
+    },
+    {
+        title: '미리내공원1',
+        latlng: new daum.maps.LatLng(33.459055, 126.478797)
+    },
+    {
+        title: '미리내공원2',
+        latlng: new daum.maps.LatLng(33.459055, 126.478797)
+    },
+    {
+        title: '칠성로 차없는거리',
+        latlng: new daum.maps.LatLng(33.516536, 126.525281)
+    },
+    {
+        title: '노형신비의도로 (소정도채비옆)',
+        latlng: new daum.maps.LatLng(33.451863, 126.487223)
+    },
+    {
+        title: '용담공원(용연)',
+        latlng: new daum.maps.LatLng(33.512867, 126.515090)
+    },
+    {
+        title: '삼무공원',
+        latlng: new daum.maps.LatLng(33.491505, 126.492015)
+    },
+    {
+        title: '탑동1공원(서측)',
+        latlng: new daum.maps.LatLng(33.518675, 126.520255)
+    },
+    {
+        title: '탑동1공원(동측)',
+        latlng: new daum.maps.LatLng(33.518179, 126.524185)
+    },
+    {
+        title: '이도공원',
+        latlng: new daum.maps.LatLng(33.497316, 126.544174)
+    },
+    {
+        title: '수운공원',
+        latlng: new daum.maps.LatLng(33.500283, 126.540042)
+    },
+    {
+        title: '39호어린이공원',
+        latlng: new daum.maps.LatLng(33.500147, 126.538051)
+    },
+    {
+        title: '40호어린이공원',
+        latlng: new daum.maps.LatLng(33.497528, 126.540823)
+    },
+    {
+        title: '41호어린이공원',
+        latlng: new daum.maps.LatLng(33.502267, 126.544827)
+    },
+    {
+        title: '42호어린이공원',
+        latlng: new daum.maps.LatLng(33.500072, 126.547273)
+    },
+    {
+        title: '남조봉공원(민오름)',
+        latlng: new daum.maps.LatLng(33.486239, 126.492702)
+    },
+    {
+        title: '신산공원(동쪽)',
+        latlng: new daum.maps.LatLng(33.505257, 126.532470)
+    },
+    {
+        title: '신산공원(게이트볼장)',
+        latlng: new daum.maps.LatLng(33.507551, 126.532116)
+    },
+    {
+        title: '사라봉공원(서측)',
+        latlng: new daum.maps.LatLng(33.518083, 126.546054)
+    },
+    {
+        title: '사라봉공원(정상)',
+        latlng: new daum.maps.LatLng(33.518083, 126.546054)
+    },
+    {
+        title: '사라봉공원(동축)',
+        latlng: new daum.maps.LatLng(33.518083, 126.546054)
+    },
+    {
+        title: '노형근린공원',
+        latlng: new daum.maps.LatLng(33.480139, 126.482536)
+    },
+    {
+        title: '연동공원',
+        latlng: new daum.maps.LatLng(33.482368, 126.488373)
+    },
+    {
+        title: '노형제1공원',
+        latlng: new daum.maps.LatLng(33.486646, 126.469511)
+    },
+    {
+        title: '노형제2공원',
+        latlng: new daum.maps.LatLng(33.489457, 126.471923)
+    },
+    {
+        title: '시민복지타운공원',
+        latlng: new daum.maps.LatLng(33.489403, 126.526308)
+    },
+    {
+        title: '원당봉공원',
+        latlng: new daum.maps.LatLng(33.526296, 126.598385)
+    },
+    {
+        title: '한마음공원',
+        latlng: new daum.maps.LatLng(33.496420, 126.546213)
+    },
 ];
 
 // 마커 이미지의 이미지 주소입니다
@@ -37,7 +213,7 @@ for (var i = 0; i < positions.length; i ++) {
     var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize);
 
     // 마커를 생성합니다
-    var marker = new daum.maps.Marker({
+    var tMarkers = new daum.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
@@ -45,52 +221,306 @@ for (var i = 0; i < positions.length; i ++) {
     });
 }
 
-// HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-if (navigator.geolocation) {
+    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+    if (navigator.geolocation) {
 
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
+        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        navigator.geolocation.getCurrentPosition(function(position) {
 
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
+            var lat = position.coords.latitude, // 위도
+                lon = position.coords.longitude; // 경도
 
-        var locPosition = new daum.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+            var locPosition = new daum.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+                message = '<div style="padding:5px;">현재위치입니다!</div>'; // 인포윈도우에 표시될 내용입니다
 
-        // 마커와 인포윈도우를 표시합니다
+            // 마커와 인포윈도우를 표시합니다
+            displayMarker(locPosition, message);
+
+          });
+
+    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+        var locPosition = new daum.maps.LatLng(33.450701, 126.570667),
+            message = 'geolocation을 사용할수 없어요..'
+
         displayMarker(locPosition, message);
+    }
 
-      });
+    // 지도에 마커와 인포윈도우를 표시하는 함수입니다
+    function displayMarker(locPosition, message) {
 
-} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+        // 마커를 생성합니다
+        var marker = new daum.maps.Marker({
+            map: map,
+            position: locPosition
+        });
 
-    var locPosition = new daum.maps.LatLng(33.450701, 126.570667),
-        message = 'geolocation을 사용할수 없어요..'
+        var iwContent = message, // 인포윈도우에 표시할 내용
+            iwRemoveable = true;
 
-    displayMarker(locPosition, message);
+        // 인포윈도우를 생성합니다
+        var infowindow = new daum.maps.InfoWindow({
+            content : iwContent,
+            removable : iwRemoveable
+        });
+
+        // 인포윈도우를 마커위에 표시합니다
+        infowindow.open(map, marker);
+
+        // 지도 중심좌표를 접속위치로 변경합니다
+        map.setCenter(locPosition);
+    }
+
+var drawingFlag = false; // 선이 그려지고 있는 상태를 가지고 있을 변수입니다
+var moveLine; // 선이 그려지고 있을때 마우스 움직임에 따라 그려질 선 객체 입니다
+var clickLine // 마우스로 클릭한 좌표로 그려질 선 객체입니다
+var distanceOverlay; // 선의 거리정보를 표시할 커스텀오버레이 입니다
+var dots = {}; // 선이 그려지고 있을때 클릭할 때마다 클릭 지점과 거리를 표시하는 커스텀 오버레이 배열입니다.
+
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 선 그리기가 시작됩니다 그려진 선이 있으면 지우고 다시 그립니다
+daum.maps.event.addListener(map, 'click', function(mouseEvent) {
+
+    // 마우스로 클릭한 위치입니다
+    var clickPosition = mouseEvent.latLng;
+
+    // 지도 클릭이벤트가 발생했는데 선을 그리고있는 상태가 아니면
+    if (!drawingFlag) {
+
+        // 상태를 true로, 선이 그리고있는 상태로 변경합니다
+        drawingFlag = true;
+
+        // 지도 위에 선이 표시되고 있다면 지도에서 제거합니다
+        deleteClickLine();
+
+        // 지도 위에 커스텀오버레이가 표시되고 있다면 지도에서 제거합니다
+        deleteDistnce();
+
+        // 지도 위에 선을 그리기 위해 클릭한 지점과 해당 지점의 거리정보가 표시되고 있다면 지도에서 제거합니다
+        deleteCircleDot();
+
+        // 클릭한 위치를 기준으로 선을 생성하고 지도위에 표시합니다
+        clickLine = new daum.maps.Polyline({
+            map: map, // 선을 표시할 지도입니다
+            path: [clickPosition], // 선을 구성하는 좌표 배열입니다 클릭한 위치를 넣어줍니다
+            strokeWeight: 3, // 선의 두께입니다
+            strokeColor: '#db4040', // 선의 색깔입니다
+            strokeOpacity: 1, // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'solid' // 선의 스타일입니다
+        });
+
+        // 선이 그려지고 있을 때 마우스 움직임에 따라 선이 그려질 위치를 표시할 선을 생성합니다
+        moveLine = new daum.maps.Polyline({
+            strokeWeight: 3, // 선의 두께입니다
+            strokeColor: '#db4040', // 선의 색깔입니다
+            strokeOpacity: 0.5, // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'solid' // 선의 스타일입니다
+        });
+
+        // 클릭한 지점에 대한 정보를 지도에 표시합니다
+        displayCircleDot(clickPosition, 0);
+
+
+    } else { // 선이 그려지고 있는 상태이면
+
+        // 그려지고 있는 선의 좌표 배열을 얻어옵니다
+        var path = clickLine.getPath();
+
+        // 좌표 배열에 클릭한 위치를 추가합니다
+        path.push(clickPosition);
+
+        // 다시 선에 좌표 배열을 설정하여 클릭 위치까지 선을 그리도록 설정합니다
+        clickLine.setPath(path);
+
+        var distance = Math.round(clickLine.getLength());
+        displayCircleDot(clickPosition, distance);
+    }
+});
+
+// 지도에 마우스무브 이벤트를 등록합니다
+// 선을 그리고있는 상태에서 마우스무브 이벤트가 발생하면 그려질 선의 위치를 동적으로 보여주도록 합니다
+daum.maps.event.addListener(map, 'mousemove', function (mouseEvent) {
+
+    // 지도 마우스무브 이벤트가 발생했는데 선을 그리고있는 상태이면
+    if (drawingFlag){
+
+        // 마우스 커서의 현재 위치를 얻어옵니다
+        var mousePosition = mouseEvent.latLng;
+
+        // 마우스 클릭으로 그려진 선의 좌표 배열을 얻어옵니다
+        var path = clickLine.getPath();
+
+        // 마우스 클릭으로 그려진 마지막 좌표와 마우스 커서 위치의 좌표로 선을 표시합니다
+        var movepath = [path[path.length-1], mousePosition];
+        moveLine.setPath(movepath);
+        moveLine.setMap(map);
+
+        var distance = Math.round(clickLine.getLength() + moveLine.getLength()), // 선의 총 거리를 계산합니다
+            content = '<div class="dotOverlay distanceInfo">총거리 <span class="number">' + distance + '</span>m</div>'; // 커스텀오버레이에 추가될 내용입니다
+
+        // 거리정보를 지도에 표시합니다
+        showDistance(content, mousePosition);
+    }
+});
+
+// 지도에 마우스 오른쪽 클릭 이벤트를 등록합니다
+// 선을 그리고있는 상태에서 마우스 오른쪽 클릭 이벤트가 발생하면 선 그리기를 종료합니다
+daum.maps.event.addListener(map, 'rightclick', function (mouseEvent) {
+
+    // 지도 오른쪽 클릭 이벤트가 발생했는데 선을 그리고있는 상태이면
+    if (drawingFlag) {
+
+        // 마우스무브로 그려진 선은 지도에서 제거합니다
+        moveLine.setMap(null);
+        moveLine = null;
+
+        // 마우스 클릭으로 그린 선의 좌표 배열을 얻어옵니다
+        var path = clickLine.getPath();
+
+        // 선을 구성하는 좌표의 개수가 2개 이상이면
+        if (path.length > 1) {
+
+            // 마지막 클릭 지점에 대한 거리 정보 커스텀 오버레이를 지웁니다
+            if (dots[dots.length-1].distance) {
+                dots[dots.length-1].distance.setMap(null);
+                dots[dots.length-1].distance = null;
+            }
+
+            var distance = Math.round(clickLine.getLength()), // 선의 총 거리를 계산합니다
+                content = getTimeHTML(distance); // 커스텀오버레이에 추가될 내용입니다
+
+            // 그려진 선의 거리정보를 지도에 표시합니다
+            showDistance(content, path[path.length-1]);
+
+        } else {
+
+            // 선을 구성하는 좌표의 개수가 1개 이하이면
+            // 지도에 표시되고 있는 선과 정보들을 지도에서 제거합니다.
+            deleteClickLine();
+            deleteCircleDot();
+            deleteDistnce();
+
+        }
+
+        // 상태를 false로, 그리지 않고 있는 상태로 변경합니다
+        drawingFlag = false;
+    }
+});
+
+// 클릭으로 그려진 선을 지도에서 제거하는 함수입니다
+function deleteClickLine() {
+    if (clickLine) {
+        clickLine.setMap(null);
+        clickLine = null;
+    }
 }
 
-// 지도에 마커와 인포윈도우를 표시하는 함수입니다
-function displayMarker(locPosition, message) {
+// 마우스 드래그로 그려지고 있는 선의 총거리 정보를 표시하거
+// 마우스 오른쪽 클릭으로 선 그리가 종료됐을 때 선의 정보를 표시하는 커스텀 오버레이를 생성하고 지도에 표시하는 함수입니다
+function showDistance(content, position) {
 
-    // 마커를 생성합니다
-    var marker = new daum.maps.Marker({
-        map: map,
-        position: locPosition
+    if (distanceOverlay) { // 커스텀오버레이가 생성된 상태이면
+
+        // 커스텀 오버레이의 위치와 표시할 내용을 설정합니다
+        distanceOverlay.setPosition(position);
+        distanceOverlay.setContent(content);
+
+    } else { // 커스텀 오버레이가 생성되지 않은 상태이면
+
+        // 커스텀 오버레이를 생성하고 지도에 표시합니다
+        distanceOverlay = new daum.maps.CustomOverlay({
+            map: map, // 커스텀오버레이를 표시할 지도입니다
+            content: content,  // 커스텀오버레이에 표시할 내용입니다
+            position: position, // 커스텀오버레이를 표시할 위치입니다.
+            xAnchor: 0,
+            yAnchor: 0,
+            zIndex: 3
+        });
+    }
+}
+
+// 그려지고 있는 선의 총거리 정보와
+// 선 그리가 종료됐을 때 선의 정보를 표시하는 커스텀 오버레이를 삭제하는 함수입니다
+function deleteDistnce () {
+    if (distanceOverlay) {
+        distanceOverlay.setMap(null);
+        distanceOverlay = null;
+    }
+}
+
+// 선이 그려지고 있는 상태일 때 지도를 클릭하면 호출하여
+// 클릭 지점에 대한 정보 (동그라미와 클릭 지점까지의 총거리)를 표출하는 함수입니다
+function displayCircleDot(position, distance) {
+
+    // 클릭 지점을 표시할 빨간 동그라미 커스텀오버레이를 생성합니다
+    var circleOverlay = new daum.maps.CustomOverlay({
+        content: '<span class="dot"></span>',
+        position: position,
+        zIndex: 1
     });
 
-    var iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true;
+    // 지도에 표시합니다
+    circleOverlay.setMap(map);
 
-    // 인포윈도우를 생성합니다
-    var infowindow = new daum.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-    });
+    if (distance > 0) {
+        // 클릭한 지점까지의 그려진 선의 총 거리를 표시할 커스텀 오버레이를 생성합니다
+        var distanceOverlay = new daum.maps.CustomOverlay({
+            content: '<div class="dotOverlay">거리 <span class="number">' + distance + '</span>m</div>',
+            position: position,
+            yAnchor: 1,
+            zIndex: 2
+        });
 
-    // 인포윈도우를 마커위에 표시합니다
-    infowindow.open(map, marker);
+        // 지도에 표시합니다
+        distanceOverlay.setMap(map);
+    }
 
-    // 지도 중심좌표를 접속위치로 변경합니다
-    map.setCenter(locPosition);
+    // 배열에 추가합니다
+    dots.push({circle:circleOverlay, distance: distanceOverlay});
+}
+
+// 클릭 지점에 대한 정보 (동그라미와 클릭 지점까지의 총거리)를 지도에서 모두 제거하는 함수입니다
+function deleteCircleDot() {
+    var i;
+
+    for ( i = 0; i < dots.length; i++ ){
+        if (dots[i].circle) {
+            dots[i].circle.setMap(null);
+        }
+
+        if (dots[i].distance) {
+            dots[i].distance.setMap(null);
+        }
+    }
+
+    dots = [];
+}
+
+// 마우스 우클릭 하여 선 그리기가 종료됐을 때 호출하여
+// 그려진 선의 총거리 정보와 거리에 대한 도보, 자전거 시간을 계산하여
+// HTML Content를 만들어 리턴하는 함수입니다
+function getTimeHTML(distance) {
+
+    // 도보의 시속은 평균 4km/h 이고 도보의 분속은 67m/min입니다
+    var walkkTime = distance / 67 | 0;
+    var walkHour = '', walkMin = '';
+
+    // 계산한 도보 시간이 60분 보다 크면 시간으로 표시합니다
+    if (walkkTime > 60) {
+        walkHour = '<span class="number">' + Math.floor(walkkTime / 60) + '</span>시간 '
+    }
+    walkMin = '<span class="number">' + walkkTime % 60 + '</span>분'
+
+
+    // 거리와 도보 시간, 자전거 시간을 가지고 HTML Content를 만들어 리턴합니다
+    var content = '<ul class="dotOverlay distanceInfo">';
+    content += '    <li>';
+    content += '        <span class="label">총거리</span><span class="number">' + distance + '</span>m';
+    content += '    </li>';
+    content += '    <li>';
+    content += '        <span class="label">걸어서</span>' + walkHour + walkMin;
+    content += '    </li>';
+    content += '</ul>'
+
+    return content;
 }
